b3bab39470e676d1f7e54abf94f57ee72cb410d0
azu <azuciao@gmail.com>
2019-06-02 17:09:31 +0900

    feat(fs): add cwd option

--- src/fsAdaptor.ts
+++ src/fsAdaptor.ts
@@ -3,28 +3,34 @@ import * as fs from "fs";
 import * as path from "path";
 import * as util from "util";
 
-export const createFsAdaptor = (): KoreFileAdaptor => {
+export interface FsAdaptorOptions {
+    cwd?: string
+}
+
+export const createFsAdaptor = (options?: FsAdaptorOptions): KoreFileAdaptor => {
     const readFile = util.promisify(fs.readFile);
     const writeFile = util.promisify(fs.writeFile);
     const unlinkFile = util.promisify(fs.unlink);
     const mkdir = util.promisify(fs.mkdir);
+    const cwd = options && options.cwd ? options.cwd : process.cwd();
     return {
         readFile(filePath: string): Promise<string> {
-            return readFile(filePath, "utf-8");
+            return readFile(path.resolve(cwd, filePath), "utf-8");
         },
         async writeFile(filePath: string, content: string | ArrayBuffer | Buffer): Promise<void> {
-            const dir = path.dirname(filePath);
+            const resolvedFilePath = path.resolve(cwd, filePath);
+            const dir = path.dirname(resolvedFilePath);
             await mkdir(dir, {
                 recursive: true
             });
             if (Buffer.isBuffer(content) || ArrayBuffer.isView(content)) {
-                return writeFile(filePath, content);
+                return writeFile(resolvedFilePath, content);
             } else {
-                return writeFile(filePath, content, "utf-8");
+                return writeFile(resolvedFilePath, content, "utf-8");
             }
         },
         deleteFile(filePath: string): Promise<void> {
-            return unlinkFile(filePath);
+            return unlinkFile(path.resolve(cwd, filePath));
         }
     };
 };
